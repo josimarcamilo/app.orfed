2b62998364d04c293d18ba487164fec7a0300b58
Josimar Camilo <josimarifmg@gmail.com>
2024-06-12 14:17:37 -0300

    Instalando breeze com livewire

---
+++ resources/views/livewire/accounts.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>Minhas contas</h1>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Livewire\Accounts;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/accounts', Accounts::class)->middleware('auth');
+
+Route::view('/', 'welcome');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
